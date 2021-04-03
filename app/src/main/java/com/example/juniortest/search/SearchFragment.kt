097128a7e4e0c1ae3d3358b3b0b290d3557c5099
Name <Name@cbs.com>
2021-04-04 01:10:11 +0300

    removed unnecessary library

--- app/src/main/java/com/example/juniortest/search/SearchFragment.kt
+++ app/src/main/java/com/example/juniortest/search/SearchFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
